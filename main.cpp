05858e3efe4bc4be377bbd14ccaba78d031f43df
grbcp5 <grbcp5@mst.edu>
2017-11-07 15:33:05 -0600

    Demonstrate allocated memory can be used

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
-
-
+/*******************************************************************************
+ *
+ * File:
+ *   main.cpp
+ *
+ * Author:
+ *   Grant Broadwater
+ *
+ * Description:
+ *   Main class to test different allocators
+ *
+ ******************************************************************************/
 
 #include <iostream>
 
@@ -18,34 +28,40 @@ int main() {
   char *v3;
 
   /* Local variables */
-  FirstFit *allocator;
+  Allocator *allocator;
 
   /* Create new allocator */
   allocator = new FirstFit();
-  cout << ( *allocator ) << endl;
+  cout << ( *dynamic_cast<FirstFit *>( allocator )) << endl;
 
   /* Allocate 10 bytes */
   cout << "Allocate 10 bytes" << endl;
   v1 = ( char * ) allocator->alloc( 10 );
-  cout << ( *allocator ) << endl;
+  cout << ( *dynamic_cast<FirstFit *>( allocator )) << endl;
+
+  /* Use allocated memory */
+  strcpy( v1, "Hello" );
+  cout << "V1: " << v1 << endl << endl;
 
   /* Allocate 14 bytes */
   cout << "Allocate 14 bytes" << endl;
   v2 = ( char * ) allocator->alloc( 14 );
-  cout << ( *allocator ) << endl;
+  cout << ( *dynamic_cast<FirstFit *>( allocator )) << endl;
 
   /* Allocate 14 bytes */
   cout << "Allocate 32 bytes" << endl;
   v3 = ( char * ) allocator->alloc( 32 );
-  cout << ( *allocator ) << endl;
+  cout << ( *dynamic_cast<FirstFit *>( allocator )) << endl;
 
-
-  cout << "Dealloc 14 bytes" << endl;
+  /* Deallocate second block */
+  cout << "Dealloc second block" << endl;
   allocator->dealloc( v2 );
-  cout << ( *allocator ) << endl;
+  cout << ( *dynamic_cast<FirstFit *>( allocator )) << endl;
 
+  /* Allocate 10 bytes */
   cout << "Alloc 10 bytes" << endl;
   v2 = ( char * ) allocator->alloc( 10 );
-  cout << ( *allocator ) << endl;
+  cout << ( *dynamic_cast<FirstFit *>( allocator )) << endl;
 
+  delete ( allocator );
 }
