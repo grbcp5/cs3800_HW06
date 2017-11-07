252e4e62814e1db4ed02f6f3860de7a0436a2549
grbcp5 <grbcp5@mst.edu>
2017-11-07 13:23:57 -0600

    Make memory block a linked list node

--- MemoryBlock.h
+++ MemoryBlock.h
@@ -7,28 +7,62 @@
 
 #include <stdatomic.h>
 
+struct MemoryBlock;
+
+typedef MemoryBlock *MemoryBlockPtr;
+
 struct MemoryBlock {
     size_t size;
     void *start_position;
     bool occupied;
+    MemoryBlock *before;
+    MemoryBlock *next;
 
     MemoryBlock() :
         size( 0 ),
         start_position( NULL ),
-        occupied( false ) {}
+        occupied( false ),
+        before( NULL ),
+        next( NULL ) {}
 
     MemoryBlock( const size_t s, void *pos, bool isOccupied ) :
         size( s ),
         start_position( pos ),
-        occupied( isOccupied ) {}
+        occupied( isOccupied ),
+        before( NULL ),
+        next( NULL ) {}
+
+    MemoryBlock(
+        const size_t s,
+        void *pos,
+        bool o,
+        MemoryBlock *b,
+        MemoryBlock *n
+    ) :
+        size( s ),
+        start_position( pos ),
+        occupied( o ),
+        before( b ),
+        next( n ) {}
 
     ~MemoryBlock() {
 
       size = 0;
       occupied = false;
+
       if ( start_position != NULL ) {
         delete ( start_position );
         start_position = NULL;
+      }
+
+      if ( before != NULL ) {
+        delete ( before );
+        before = NULL;
+      }
+
+      if ( next != NULL ) {
+        delete ( next );
+        next = NULL;
       }
 
     }
