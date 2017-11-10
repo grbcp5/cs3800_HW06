6f2ef80e2f3a7f77150cde4f8dd748a872fa8817
grbcp5 <grbcp5@mst.edu>
2017-11-10 13:56:27 -0600

    Move test code to seperate file

---
+++ test.h
@@ -0,0 +1,16 @@
+//
+// Created by Grant Broadwater on 11/10/17.
+//
+
+#ifndef HW06_TEST_H
+#define HW06_TEST_H
+
+void testFirstFit();
+
+void testNextFit();
+
+void testBestFit();
+
+void testInput();
+
+#endif //HW06_TEST_H
